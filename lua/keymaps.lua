575574c46946db93a695cc7fb5c59316145f08f3
Kevin <kevinhle@outlook.com>
2023-04-29 20:08:08 -0500

    add map leader z as zR unfold all

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -55,3 +55,5 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+keymap.set("n", "<leader>z", "zR") -- expand all code folding 
+
